d5d013f5a9c28ea7d44944a69f8101271dd378e9
Reece_Collett <rcollett@spartaglobal.com>
2018-08-06 16:51:07 +0100

    Added variables

---
+++ js_calculator/calc.js
@@ -0,0 +1,7 @@
+var num1 = prompt("Please insert your first number");
+var operator = prompt("What would you like to do? (+, -, *, /)");
+var num2 = prompt("Please insert your second number");
+
+if (operator == "+")
+{
+}
