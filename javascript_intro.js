92f7720d61fb1e1d922b21f139db2f196d6ded49
Reece_Collett <rcollett@spartaglobal.com>
2018-08-06 14:12:33 +0100

    Made a variable and printed it to console

---
+++ javascript_intro.js
@@ -0,0 +1,13 @@
+// First variable
+var a = 5;
+
+/*
+var - Declaring that we want to create a variable
+a - Identifier
+= - Assignment
+5 - Value
+; - End of the line
+*/
+
+// Output the variable
+console.log(a);
