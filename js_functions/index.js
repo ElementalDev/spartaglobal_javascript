072f00d3a2a30837a25f1dfa842cc55d5f7f4e54
Reece_Collett <rcollett@spartaglobal.com>
2018-08-07 11:26:59 +0100

    Created first function

---
+++ js_functions/index.js
@@ -0,0 +1,15 @@
+// This is a JS file to demostrate functions
+
+// DRY - (D)on't (R)epeat (Y)ourself
+
+//Writing basic functions
+var result = 5 + 10;
+var result2 = 1 + 2;
+
+console.log(result);
+
+// Declaring a function
+function addNumbers(/* Parentheses for parameters */) {
+  var functionResult = 5 + 10;
+  console.log(function_result);
+}
