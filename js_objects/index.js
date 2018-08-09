9b6feeeb2a4f78f72a2b29533a7bc996e159c147
Reece_Collett <rcollett@spartaglobal.com>
2018-08-09 11:43:59 +0100

    Completed object notes

--- js_objects/index.js
+++ js_objects/index.js
@@ -1,1 +1,58 @@
-// This document will demontrate Objects
+  //This document will demontrate Objects in Javascript
+
+  //FIRST OBJECT
+
+  // var person = new Object();
+  //
+  // person.name = "Reece";
+  // person.age = 20;
+  // person.employed = true;
+  // person.friend = ["Bill", "Ben", "The Flower Pot Men"];
+  // person.sayHello = function() {
+  //   console.log("Say Hello");
+  // }
+  //
+  // console.log(person);
+
+  // //FIRST CONSTRUCTOR
+  //
+  // var Person = function() {
+  //   this.name = "no name";
+  //   this.age = 0;
+  //   this.employed = false;
+  //   this.friend = [];
+  //   this.sayHello = function() {
+  //     console.log("Hi my name is" + this.name);
+  //   }
+  // }
+  //
+  // //CREATE AN INSTANCE
+  // var person1 = new Person();
+  // var person2 = new Person();
+  // var person3 = new Person();
+  //
+  // console.log(person1.name);
+  // console.log(person2.name);
+
+  // var Person = function(name, age, employed) {
+  //   this.name = name;
+  //   this.age = age;
+  //   this.employed = employed;
+  //   this.friend = [];
+  //   this.sayHello = function() {
+  //     console.log("Hi my name is " + this.name);
+  //   }
+  // }
+  //
+  // var person1 = new Person("Reece", 20, true);
+  // var person2 = new Person("Rich", 75, true);
+  // var person3 = new Person("Bob", 50, true);
+  //
+  //
+  // //Properties
+  // console.log(person1.name);
+  // console.log(person2.name);
+  // console.log(person3.name);
+  //
+  // //Methods
+  // person1.sayHello();
