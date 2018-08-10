603ca09b977c5192058d55e09005e282f38816f0
Reece_Collett <rcollett@spartaglobal.com>
2018-08-10 10:31:10 +0100

    Inputted function that waits for DOM to load before running js

--- js_domEvents/DOM.js
+++ js_domEvents/DOM.js
@@ -1,45 +1,13 @@
-// This will go through the features of DOM
+// This will go through the features of DOM events
 
-// -- GetElementById --
+//Able to keep JS at the top - Waits for DOM to load
 
-var wrapperDiv = document.getElementById("wrapper");
-wrapperDiv.style.backgroundColor = "blue";
+document.addEventListener("DOMContentLoaded", function() {
 
-// -- GetElementsByTagName --
+  //Access the button
 
-var lis = document.getElementsByTagName("li"); //Returns an array of elements with tag of li
-lis[1].innerHTML = "Im an LI and I've changed";
+  var myButton = document.getElementById("myButton");
 
-// -- querySelector --
+  console.log(myButton);
 
-var selected = document.querySelectorAll("li.selected"); //Returns an array of lists where class = selected
-for (var i = 0; i < selected.length; i++) {
- selected[i].style.color = "red"
-}
-
-// -- Creating elements --
-
-var ptag = document.createElement("p");
-
-ptag.innerHTML = "A brand new p tag"
-
-document.body.appendChild(ptag); //Will insert in the bottom of the HTML
-
-var ul = document.getElementById("list");
-var listItem = document.createElement("li");
-listItem.innerHTML = "New list item JS";
-
-ul.appendChild(listItem);//Will insert at the bottom of the ul tag
-
-//Turn every other list item into a different color
-
-for (var i = 0; i < lis.length; i++) {
-  var className = i % 2 == 0 ? "even" : ""; //Mini if statement (Ternery)
-  var li = lis[i];
-
-  li.className = className;
-}
-
-var countSpan = document.getElementById("count");
-
-countSpan.innerHTML = lis.length;
+})
