3bfe7e33121aa6ccb947bb3f8d67395d8a17f293
Reece_Collett <rcollett@spartaglobal.com>
2018-08-10 14:43:14 +0100

    Added new files to Ajax

--- Ajax/Ajax.js
+++
@@ -1,1 +0,0 @@
-//This document will demonstrate Ajax
---
+++ Ajax/index.js
@@ -0,0 +1,36 @@
+$(function(){
+
+  // on dom ready
+
+  // // create a new instance
+  // var request = new XMLHttpRequest();
+
+  // // setup a GET request to the chuck norris api
+  // request.open("GET", "http://api.icndb.com/jokes/random");
+
+  // // set the callback function
+  // request.addEventListener("load", function(){
+
+  //   var data = JSON.parse(this.responseText);
+
+  //   console.log(data);
+
+  // });
+
+  // // send the request
+  // request.send();
+
+  setInterval(function(){
+
+    $.get('http://api.icndb.com/jokes/random' , function(data){
+      // grab the joke from the data
+      var joke = data.value.joke;
+      
+      // grab the joke title element and set the html
+      $('#joke').html(joke);
+    });
+
+  } , 10000);
+
+
+});
