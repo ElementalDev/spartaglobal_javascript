c7ac27e1dd65e97300bf97fdf85592cf4a1d58e6
Reece_Collett <rcollett@spartaglobal.com>
2018-08-10 11:00:54 +0100

    Completed code for DOM Events

--- js_domEvents/DOM.js
+++ js_domEvents/DOM.js
@@ -1,13 +1,26 @@
 // This will go through the features of DOM events
 
-//Able to keep JS at the top - Waits for DOM to load
+//Able to keep JS at the top - Waits for DOM(HTML) to load
 
 document.addEventListener("DOMContentLoaded", function() {
 
   //Access the button
-
   var myButton = document.getElementById("myButton");
 
-  console.log(myButton);
+  //Add event listener for myButton
+  myButton.addEventListener("click", handleClick);
 
-})
+  //handleClick function
+  function handleClick() {
+    console.log("Button was clicked");
+  }
+
+  //Access the Form
+  var myForm = document.getElementById("myForm");
+
+  //Add Listener to Form
+  myForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    console.log("The form was submitted");
+  })
+});
